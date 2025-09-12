36ceeed2c0b6b67e18b8e14d9d903ccd039d9a81
raheelanwaar03 <raheelanwaar0316@gmail.com>
2025-09-13 02:21:55 +0500

    new updation

--- resources/views/admin/dashboard/approvedUsers.blade.php
+++ resources/views/admin/dashboard/approvedUsers.blade.php
@@ -55,32 +55,16 @@
                                 <table id="dataTable" class="table table-hover table-stripped">
                                     <thead>
                                         <tr>
-                                            <th>Id</th>
                                             <th>Name</th>
-                                            <th>Email</th>
                                             <th>Balance</th>
-                                            <th>Phone</th>
-                                            <th>Level</th>
-                                            <th>Plan</th>
-                                            <th>Tid</th>
-                                            <th>Status</th>
-                                            <th>Date</th>
                                             <th>Action</th>
                                         </tr>
                                     </thead>
                                     <tbody>
                                         @foreach ($users as $user)
                                             <tr>
-                                                <td>{{ $user->id }}</td>
                                                 <td>{{ $user->name }}</td>
-                                                <td>{{ $user->email }}</td>
                                                 <td>{{ $user->balance }}</td>
-                                                <td>{{ $user->phone }}</td>
-                                                <td>{{ $user->level }}</td>
-                                                <td>{{ $user->plan }}</td>
-                                                <td>{{ $user->trxIds->tid ?? 'UnpaidUser' }}</td>
-                                                <td>{{ $user->status }}</td>
-                                                <td>{{ $user->created_at }}</td>
                                                 <td>
                                                     <a href="{{ route('Admin.Approve.User.Account.Request', ['id' => $user->id]) }}"
                                                         class="btn btn-sm btn-success">Approved</a>
