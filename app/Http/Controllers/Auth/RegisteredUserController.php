645129809012c604e07f1c249e2943b5cb717846
raheelanwaar03 <raheelanwaar0316@gmail.com>
2025-09-13 23:17:21 +0500

    register bounce ended

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -50,7 +50,6 @@ class RegisteredUserController extends Controller
             'email' => $request->email,
             'phone' => $request->phone,
             'plan' => $request->plan,
-            'balance' => '25',
             'referal' => $request->referal,
             'password' => Hash::make($request->password),
         ]);
